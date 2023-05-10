582f385418396d84d0461bb6bcd4c8da780f6578
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-05-10 10:22:40 +0700

    add

---
+++ lib/splash_screen_package.dart
@@ -0,0 +1,66 @@
+library splash_screen_package;
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  final Function(BuildContext context) checkLoginStatus;
+  String? title;
+
+  SplashScreen({
+    Key? key,
+    this.title,
+    required this.checkLoginStatus
+  }) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(seconds: 2), () => widget.checkLoginStatus(context));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'packages/splash_screen_package/assets/images/background.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Container(width: 120,
+                  height: 120,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      image: DecorationImage(
+                        image: AssetImage('packages/splash_screen_package/assets/images/bird.png',),
+                      )
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Text(
+                //"This is splash,x = ${this.x}",
+                widget.title ?? '',
+                style: const TextStyle(fontSize: 25, color: Colors.white),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
---
+++ test/splash_screen_package_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:splash_screen_package/splash_screen_package.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+  });
+}
